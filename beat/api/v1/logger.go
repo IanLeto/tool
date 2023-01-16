d6cc004dde366bc686303ff0d93165762e6492e9
ian.liu <ian.liu@ucloud.cn>
2023-01-16 10:07:23 +0800

    hello

--- beat/api/v1/logger.go
+++ beat/api/v1/logger.go
@@ -2,10 +2,20 @@ package v1
 
 import "github.com/gogf/gf/v2/frame/g"
 
+type BaseResponseInfo struct {
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
+	Code    int         `json:"code"`
+}
+
 type GetLoggerReq struct {
 	g.Meta      `path:"/log" method:"get" tags:"fortest" summary:"test summary"`
 	ClusterID   string
 	ClusterName string
 	ProjectID   string
 	TimeRange   []int
+	ID          string
+}
+
+type GetLoggerRes struct {
 }
--- beat/go.mod
+++ beat/go.mod
@@ -2,4 +2,7 @@ module beat
 
 go 1.15
 
-require github.com/gogf/gf/v2 v2.0.6
+require (
+	github.com/elastic/go-elasticsearch/v7 v7.17.7
+	github.com/gogf/gf/v2 v2.0.6
+)
