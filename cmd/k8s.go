548fae337324e2fabf979e0543cc05c4c4da5a0c
ian.liu <ian.liu@ucloud.cn>
2023-08-15 16:40:14 +0800

    master

--- cmd/k8s.go
+++ cmd/k8s.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"github.com/jcmturner/gokrb5/v8/config"
 	"github.com/spf13/cobra"
+	v12 "k8s.io/api/authentication/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apiserver/pkg/apis/audit"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-
 	"log"
 )
 
@@ -47,54 +49,85 @@ var K8sCmd = &cobra.Command{
 			//item.InvolvedObject
 			fmt.Println(item.InvolvedObject)
 		}
-		//var x = v1.Event{
-		//	TypeMeta: metav1.TypeMeta{
-		//		Kind:       "",
-		//		APIVersion: "",
-		//	},
-		//	ObjectMeta: metav1.ObjectMeta{ // 事件的标准对象metadata
-		//		Name:                       "",
-		//		GenerateName:               "",
-		//		Namespace:                  "",
-		//		SelfLink:                   "",
-		//		UID:                        "",
-		//		ResourceVersion:            "",
-		//		Generation:                 0,
-		//		CreationTimestamp:          metav1.Time{},
-		//		DeletionTimestamp:          nil,
-		//		DeletionGracePeriodSeconds: nil,
-		//		Labels:                     nil,
-		//		Annotations:                nil,
-		//		OwnerReferences:            nil,
-		//		Finalizers:                 nil,
-		//		ManagedFields:              nil,
-		//	},
-		//	InvolvedObject: v1.ObjectReference{ // 这个事件对应的资源对象
-		//		Kind:            "",
-		//		Namespace:       "",
-		//		Name:            "",
-		//		UID:             "",
-		//		APIVersion:      "",
-		//		ResourceVersion: "",
-		//		FieldPath:       "",
-		//	},
-		//	Reason:  "",
-		//	Message: "",
-		//	Source: v1.EventSource{ //// The component reporting this event. Should be a short machine understandable string.
-		//		Component: "", // 生成事件的组件
-		//		Host:      "", // 生成事件的节点
-		//	},
-		//	FirstTimestamp:      metav1.Time{}, // 事件首次发生时间
-		//	LastTimestamp:       metav1.Time{}, // 事件最后一次发生事件
-		//	Count:               0,
-		//	Type:                "",
-		//	EventTime:           metav1.MicroTime{}, // 该事件首次被观察到的时间
-		//	Series:              nil,
-		//	Action:              "", // 针对该事件所指向对象的相关动作
-		//	Related:             nil,
-		//	ReportingController: "",
-		//	ReportingInstance:   "",
-		//}
+		var event = v1.Event{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "",
+				APIVersion: "",
+			},
+			ObjectMeta: metav1.ObjectMeta{ // 事件的标准对象metadata
+				Name:                       "",
+				GenerateName:               "",
+				Namespace:                  "",
+				SelfLink:                   "",
+				UID:                        "",
+				ResourceVersion:            "",
+				Generation:                 0,
+				CreationTimestamp:          metav1.Time{},
+				DeletionTimestamp:          nil,
+				DeletionGracePeriodSeconds: nil,
+				Labels:                     nil,
+				Annotations:                nil,
+				OwnerReferences:            nil,
+				Finalizers:                 nil,
+				ManagedFields:              nil,
+			},
+			InvolvedObject: v1.ObjectReference{ // 这个事件对应的资源对象
+				Kind:            "",
+				Namespace:       "",
+				Name:            "",
+				UID:             "",
+				APIVersion:      "",
+				ResourceVersion: "",
+				FieldPath:       "",
+			},
+			Reason:  "",
+			Message: "",
+			Source: v1.EventSource{ //// The component reporting this event. Should be a short machine understandable string.
+				Component: "", // 生成事件的组件
+				Host:      "", // 生成事件的节点
+			},
+			FirstTimestamp:      metav1.Time{}, // 事件首次发生时间
+			LastTimestamp:       metav1.Time{}, // 事件最后一次发生事件
+			Count:               0,
+			Type:                "",
+			EventTime:           metav1.MicroTime{}, // 该事件首次被观察到的时间
+			Series:              nil,
+			Action:              "", // 针对该事件所指向对象的相关动作
+			Related:             nil,
+			ReportingController: "",
+			ReportingInstance:   "",
+		}
+		fmt.Println(event)
+		var auditEvent = audit.Event{
+			TypeMeta:   metav1.TypeMeta{},
+			Level:      "",
+			AuditID:    "",
+			Stage:      "",
+			RequestURI: "",
+			Verb:       "",
+			User: v12.UserInfo{
+				Username: "",
+				UID:      "",
+				Groups:   nil,
+				Extra:    nil,
+			},
+			ImpersonatedUser: &v12.UserInfo{
+				Username: "",
+				UID:      "",
+				Groups:   nil,
+				Extra:    nil,
+			},
+			SourceIPs:                nil,
+			UserAgent:                "",
+			ObjectRef:                nil,
+			ResponseStatus:           nil,
+			RequestObject:            nil,
+			ResponseObject:           nil,
+			RequestReceivedTimestamp: metav1.MicroTime{}, // 抵达apisever 时间
+			StageTimestamp:           metav1.MicroTime{}, //抵达当前审计阶段时间
+			Annotations:              nil,
+		}
+		fmt.Println(auditEvent)
 	},
 }
 
--- go.mod
+++ go.mod
@@ -10,6 +10,9 @@ require (
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/viper v1.16.0
 	github.com/stretchr/testify v1.8.4
+	k8s.io/api v0.27.4
+	k8s.io/apimachinery v0.27.4
+	k8s.io/apiserver v0.27.4
 	k8s.io/client-go v0.27.4
 )
 
@@ -74,8 +77,6 @@ require (
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.27.4 // indirect
-	k8s.io/apimachinery v0.27.4 // indirect
 	k8s.io/klog/v2 v2.90.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230501164219-8b0f38b5fd1f // indirect
 	k8s.io/utils v0.0.0-20230209194617-a36077c30491 // indirect
