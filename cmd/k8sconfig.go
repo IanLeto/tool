57568a35009f4101841e88f2023333f36cb81fc5
ian.liu <ian.liu@ucloud.cn>
2024-04-03 14:05:55 +0800

    x

--- cmd/k8sconfig.go
+++ cmd/k8sconfig.go
@@ -1,41 +1,95 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
+	"io/ioutil"
 	"k8s.io/client-go/tools/clientcmd/api"
 	"os"
 )
 
-type Cluster struct {
-	Metadata struct {
-		Name string `json:"name"`
-	} `json:"metadata"`
+type Clusters struct {
+	Items []struct {
+		Metadata struct {
+			Name string `json:"name"`
+		} `json:"metadata"`
+	} `json:"items"`
+}
+
+type CustomCluster struct {
+	Server string `yaml:"server"`
+	// 其他字段你不希望显示的可以使用 `yaml:"-"` 来忽略
+	InsecureSkipTLSVerify    bool   `yaml:"-"`
+	CertificateAuthorityData []byte `yaml:"-"`
+	// 如果有其他字段也想忽略，也可以加上 `yaml:"-"`
+}
+
+type CustomContext struct {
+	Cluster   string `yaml:"cluster"`
+	AuthInfo  string `yaml:"authinfo"`
+	Namespace string `yaml:"namespace"`
+	// 忽略不需要的字段
+	// Extensions               map[string]interface{} `yaml:"-"`
 }
 
 var KubeYaml = &cobra.Command{
 	Use: "KubeYaml",
 	Run: func(cmd *cobra.Command, args []string) {
-		kubeconfig := api.Config{
+		var (
+			clusters Clusters
+		)
+		cluster, err := ioutil.ReadFile("clusters.json")
+		if err != nil {
+			fmt.Printf("Error reading clusters.json: %v\n", err)
+			return
+		}
+
+		err = json.Unmarshal(cluster, &clusters)
+		if err != nil {
+			fmt.Printf("Error unmarshaling clusters: %v\n", err)
+			return
+		}
+
+		// 使用自定义的结构体
+		kubeconfig := struct {
+			Kind           string                   `yaml:"kind"`
+			APIVersion     string                   `yaml:"apiversion"`
+			Clusters       map[string]CustomCluster `yaml:"clusters"`
+			AuthInfos      map[string]*api.AuthInfo `yaml:"authinfos"`
+			Contexts       map[string]CustomContext `yaml:"contexts"`
+			CurrentContext string                   `yaml:"currentcontext"`
+		}{
 			Kind:           "config",
 			APIVersion:     "v1",
-			Clusters:       nil,
+			Clusters:       map[string]CustomCluster{},
 			AuthInfos:      nil,
-			Contexts:       nil,
+			Contexts:       map[string]CustomContext{},
 			CurrentContext: "",
-			Extensions:     nil,
 		}
-		input, err := os.ReadFile("cluster.json")
-		NoErr(err)
-		// Marshal the kubeconfig object into YAML
+
+		for _, item := range clusters.Items {
+			clusterName := item.Metadata.Name
+			kubeconfig.Clusters[clusterName] = CustomCluster{
+				Server:                   "https://api." + clusterName + ".com",
+				CertificateAuthorityData: []byte(""),
+			}
+			kubeconfig.Contexts[clusterName] = CustomContext{
+				Cluster:   clusterName,
+				AuthInfo:  "k0110",
+				Namespace: "cpaas-system",
+			}
+		}
+
+		// 省略的 AuthInfo 结构体初始化...
+
 		yamlData, err := yaml.Marshal(&kubeconfig)
 		if err != nil {
 			fmt.Printf("Error marshaling kubeconfig: %v\n", err)
 			return
 		}
 
-		// Print to console
 		fmt.Println(string(yamlData))
 
 		// Output to a file
