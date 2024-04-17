e18bf32bcc5bd44a425e739610cfd4ebb108484e
ianleto <ccssliu@163.com>
2024-04-18 00:53:42 +0800

    x

--- cmd/updateConfig.go
+++ cmd/updateConfig.go
@@ -2,17 +2,29 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
 )
+
+var client = func() *kubernetes.Clientset {
+	kubeconfig := os.Getenv("KUBECONFIG")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	NoErr(err)
+	clientset, err := kubernetes.NewForConfig(config)
+	NoErr(err)
+	return clientset
+
+}()
 
 var BatchConfigmap = &cobra.Command{
 	Use: "update",
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do something
+
 	},
 }
 
 func init() {
 	KubeYaml.Flags().StringP("stdin", "", "", "Read from stdin")
-	KubeYaml.Flags().IntP("interval", "", 2, "Interval for something")
-	KubeYaml.Flags().IntP("size", "", 10, "Size for something")
 }
