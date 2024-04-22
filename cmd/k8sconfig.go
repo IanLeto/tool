b56e91fab95d51ca8a6c51a0359b7446f0008185
ianleto <ccssliu@163.com>
2024-04-22 12:17:25 +0800

    x

--- cmd/k8sconfig.go
+++ cmd/k8sconfig.go
@@ -1,126 +1,115 @@
 package cmd
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
-type Clusters struct {
-	Items []struct {
-		Metadata struct {
-			Name string `json:"name"`
-		} `json:"metadata"`
-	} `json:"items"`
+type KubeConfig struct {
+	APIVersion string `yaml:"apiVersion"`
+	Clusters   []struct {
+		Cluster struct {
+			InsecureSkipTLSVerify bool   `yaml:"insecure-skip-tls-verify"`
+			Server                string `yaml:"server"`
+		} `yaml:"cluster"`
+		Name string `yaml:"name"`
+	} `yaml:"clusters"`
+	Contexts []struct {
+		Context struct {
+			Cluster string `yaml:"cluster"`
+			User    string `yaml:"user"`
+		} `yaml:"context"`
+		Name string `yaml:"name"`
+	} `yaml:"contexts"`
+	CurrentContext string   `yaml:"current-context"`
+	Kind           string   `yaml:"kind"`
+	Preferences    struct{} `yaml:"preferences"`
+	Users          []struct {
+		Name string `yaml:"name"`
+		User struct {
+			Token string `yaml:"token"`
+		} `yaml:"user"`
+	} `yaml:"users"`
 }
 
-type CustomCluster struct {
-	Server string `yaml:"server"`
-	// 其他字段你不希望显示的可以使用 `yaml:"-"` 来忽略
-	InsecureSkipTLSVerify    bool   `yaml:"insecure-skip-tls-verify"`
-	CertificateAuthorityData []byte `yaml:"-"`
-	// 如果有其他字段也想忽略，也可以加上 `yaml:"-"`
+func mergeKubeConfigs(files []string) (*KubeConfig, error) {
+	var mergedConfig KubeConfig
+	for _, file := range files {
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			return nil, err
+		}
+
+		var config KubeConfig
+		if err := yaml.Unmarshal(data, &config); err != nil {
+			return nil, err
+		}
+
+		// Merge logic
+		mergedConfig.Clusters = append(mergedConfig.Clusters, config.Clusters...)
+		mergedConfig.Contexts = append(mergedConfig.Contexts, config.Contexts...)
+		mergedConfig.Users = config.Users // Assuming all users are the same
+	}
+
+	// Assuming the first file sets the general structure
+	//if len(files) > 0 {
+	//	firstConfigData, _ := ioutil.ReadFile(files[0])
+	//	err := yaml.Unmarshal(firstConfigData, &mergedConfig)
+	//	NoErr(err)
+	//}
+
+	return &mergedConfig, nil
 }
 
-type CustomContext struct {
-	Cluster   string `yaml:"cluster"`
-	User      string `yaml:"user"`
-	Namespace string `yaml:"namespace"`
-	// 忽略不需要的字段
-	// Extensions               map[string]interface{} `yaml:"-"`
-}
-type SpecCluster struct {
-	Name    string        `yaml:"name"`
-	Cluster CustomCluster `yaml:"cluster"`
-}
+func traverseAndMerge(rootDir, outputFile string) error {
+	var files []string
+	err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
 
-type SpecContext struct {
-	Name    string        `yaml:"name"`
-	Context CustomContext `yaml:"context"`
+	mergedConfig, err := mergeKubeConfigs(files)
+	if err != nil {
+		return err
+	}
+
+	outData, err := yaml.Marshal(mergedConfig)
+	if err != nil {
+		return err
+	}
+
+	if err = ioutil.WriteFile(outputFile, outData, 0644); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 var KubeYaml = &cobra.Command{
 	Use: "KubeYaml",
 	Run: func(cmd *cobra.Command, args []string) {
-		var (
-			clusters Clusters
-		)
-		cluster, err := ioutil.ReadFile("clusters.json")
-		if err != nil {
-			fmt.Printf("Error reading clusters.json: %v\n", err)
-			return
-		}
-
-		err = json.Unmarshal(cluster, &clusters)
-		if err != nil {
-			fmt.Printf("Error unmarshaling clusters: %v\n", err)
-			return
-		}
-
-		// 使用自定义的结构体
-		kubeconfig := struct {
-			Kind           string                   `yaml:"kind"`
-			APIVersion     string                   `yaml:"apiversion"`
-			Clusters       []SpecCluster            `yaml:"clusters"`
-			AuthInfos      []map[string]interface{} `yaml:"users"`
-			Contexts       []SpecContext            `yaml:"contexts"`
-			CurrentContext string                   `yaml:"current-context"`
-		}{
-			APIVersion:     "v1",
-			Kind:           "config",
-			Clusters:       []SpecCluster{},
-			AuthInfos:      []map[string]interface{}{},
-			Contexts:       []SpecContext{},
-			CurrentContext: "",
-		}
-
-		for _, item := range clusters.Items {
-			clusterName := item.Metadata.Name
-			kubeconfig.Clusters = append(kubeconfig.Clusters, SpecCluster{
-				Name: clusterName,
-				Cluster: CustomCluster{
-					Server: "https://api." + clusterName + ".com",
-				},
-			})
-			kubeconfig.Contexts = append(kubeconfig.Contexts, SpecContext{
-				Name: clusterName,
-				Context: CustomContext{
-					Cluster:   clusterName,
-					User:      "xx",
-					Namespace: "default",
-				},
-			})
-		}
-
-		// 省略的 AuthInfo 结构体初始化...
-
-		yamlData, err := yaml.Marshal(&kubeconfig)
-		if err != nil {
-			fmt.Printf("Error marshaling kubeconfig: %v\n", err)
-			return
-		}
-
-		fmt.Println(string(yamlData))
-
-		// Output to a file
-		fileName := "kubeconfig.yaml"
-		err = os.WriteFile(fileName, yamlData, 0644)
-		if err != nil {
-			fmt.Printf("Error writing kubeconfig to file: %v\n", err)
-			return
-		}
-
-		fmt.Printf("kubeconfig is written to %s\n", fileName)
+		dir, _ := cmd.Flags().GetString("dir")
+		outputFile, _ := cmd.Flags().GetString("output")
+		err := traverseAndMerge(dir, outputFile)
+		NoErr(err)
 	},
 }
 
 func init() {
-	KubeYaml.Flags().StringP("stdin", "", "", "Read from stdin")
-	KubeYaml.Flags().IntP("interval", "", 2, "Interval for something")
-	KubeYaml.Flags().IntP("size", "", 10, "Size for something")
+	KubeYaml.Flags().StringP("dir", "", "", "Read from stdin")
+	KubeYaml.Flags().StringP("output", "", "", "Read from stdin")
+
 }
 
 // It's important to note that you need to replace the initialization of the rest.Config
