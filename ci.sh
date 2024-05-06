93ba75d36e174f0577768df5112b838b47312636
ianleto <ccssliu@163.com>
2024-05-07 00:12:04 +0800

    x

--- ci.sh
+++ ci.sh
@@ -1,5 +1,5 @@
 git commit -am "x"
 export GOROOT=/home/ian/sdk/go1.22.2
-GOOS=linux GOARCH=amd64 go build -o tool main.go && \
+GOOS=linux GOARCH=amd64 go build -tags netgo -ldflags '-extldflags "-static"' -o tool main.go && \
     docker build -t ianleto/tool:$(git rev-parse --short HEAD) -f dockerfile .&&\
     docker push ianleto/tool:$(git rev-parse --short HEAD)
--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module bench
 
 go 1.21
 
-toolchain go1.22.2
+
 
 require (
 	github.com/Shopify/sarama v1.38.1
